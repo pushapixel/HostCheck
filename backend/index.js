8526cff9c8f0635d0c2ec2043c60f5acc2363c1c
Steve <sdebenport@gmail.com>
2026-06-26 17:35:13 -0500

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(session({
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   },
 }));
-
+ 
 // Passport Google OAuth
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
