f6f0311a22e026d00ea7300a0388033661a404c7
Steve <sdebenport@gmail.com>
2026-06-27 21:17:10 -0500

    Add files via upload

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
-const BASE = import.meta.env.VITE_API_URL || '';
-
 const api = async (path, options = {}) => {
-  const res = await fetch(`${BASE}${path}`, {
+  const res = await fetch(path, {
     credentials: 'include',
     headers: { 'Content-Type': 'application/json', ...options.headers },
     ...options,
